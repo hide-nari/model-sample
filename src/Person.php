12ae2f97fb126d2b92c6de03599eab3b0ce2f488
watanabeHi <hide3@mild.ocn.ne.jp>
2025-12-01 17:52:11 +0900

    Mod asynmetric accesor

--- src/Person.php
+++ src/Person.php
@@ -5,16 +5,25 @@ namespace Hidenari\ModelSample;
 class Person
 {
     public function __construct(
-        public $name
+        public string $forename
         = 'taro' {
-            get => 'Mr.'.$this->name;
-            set => $this->name = ucwords($value);
+            get => 'Mr.'.$this->forename;
+            set => $this->forename = ucwords($value);
         },
-        public $age
+        public private(set) string $surname
+        = 'yamada' {
+            get => 'Mr.'.$this->surname;
+        },
+        public int $age
         = 15 {
             get => $this->age;
             set => $this->age = $value;
         }
     ) {
+    }
+
+    public function setSurname(string $surname)
+    {
+        $this->surname = ucwords($surname);
     }
 }
