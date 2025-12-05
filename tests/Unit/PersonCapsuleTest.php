3daf0abd1d611873265b2e9e8abe1e26790c669b
watanabeHi <hide3@mild.ocn.ne.jp>
2025-12-06 01:06:48 +0900

    Mod expect logic to chain method

--- tests/Unit/PersonCapsuleTest.php
+++ tests/Unit/PersonCapsuleTest.php
@@ -4,23 +4,23 @@ use Hidenari\ModelSample\PersonCapsule;
 
 test('person capsule model no parameter', function () {
     $person = new PersonCapsule;
-    expect($person->name === 'Mr.taro')->toBeTrue();
-    expect($person->name === 'taro')->toBeFalse();
-    expect($person->age === 15)->toBeTrue();
+    expect($person->name === 'Mr.taro')->toBeTrue()
+        ->and($person->name === 'taro')->toBeFalse()
+        ->and($person->age === 15)->toBeTrue();
 
     $person->setName('jiro');
-    expect($person->name === 'Mr.Jiro')->toBeTrue();
-    expect($person->name === 'Mr.taro')->toBeFalse();
+    expect($person->name === 'Mr.Jiro')->toBeTrue()
+        ->and($person->name === 'Mr.taro')->toBeFalse();
 
     $person->setAge(20);
-    expect($person->age === 20)->toBeTrue();
-    expect($person->age === 15)->toBeFalse();
+    expect($person->age === 20)->toBeTrue()
+        ->and($person->age === 15)->toBeFalse();
 });
 
 test('person capsule with name age parameter', function () {
     $person = new PersonCapsule('jiro', 20);
-    expect($person->name === 'Mr.jiro')->toBeTrue();
-    expect($person->name === 'jiro')->toBeFalse();
-    expect($person->age === 20)->toBeTrue();
-    expect($person->age === 15)->toBeFalse();
+    expect($person->name === 'Mr.jiro')->toBeTrue()
+        ->and($person->name === 'jiro')->toBeFalse()
+        ->and($person->age === 20)->toBeTrue()
+        ->and($person->age === 15)->toBeFalse();
 });
--- tests/Unit/PersonTest.php
+++ tests/Unit/PersonTest.php
@@ -4,24 +4,24 @@ use Hidenari\ModelSample\Person;
 
 test('person model no parameter', function () {
     $person = new Person;
-    expect($person->name === 'Mr.Taro')->toBeTrue();
-    expect($person->name === 'Taro')->toBeFalse();
-    expect($person->name === 'taro')->toBeFalse();
-    expect($person->age === 15)->toBeTrue();
+    expect($person->name === 'Mr.Taro')->toBeTrue()
+        ->and($person->name === 'Taro')->toBeFalse()
+        ->and($person->name === 'taro')->toBeFalse()
+        ->and($person->age === 15)->toBeTrue();
 
     $person->name = 'jiro';
-    expect($person->name === 'Mr.Jiro')->toBeTrue();
-    expect($person->name === 'Mr.Taro')->toBeFalse();
+    expect($person->name === 'Mr.Jiro')->toBeTrue()
+        ->and($person->name === 'Mr.Taro')->toBeFalse();
 
     $person->age = 20;
-    expect($person->age === 20)->toBeTrue();
-    expect($person->age === 15)->toBeFalse();
+    expect($person->age === 20)->toBeTrue()
+        ->and($person->age === 15)->toBeFalse();
 });
 
 test('person model with name,age parameter', function () {
     $person = new Person('jiro', 20);
-    expect($person->name === 'Mr.Jiro')->toBeTrue();
-    expect($person->name === 'Jiro')->toBeFalse();
-    expect($person->name === 'jiro')->toBeFalse();
-    expect($person->age === 20)->toBeTrue();
+    expect($person->name === 'Mr.Jiro')->toBeTrue()
+        ->and($person->name === 'Jiro')->toBeFalse()
+        ->and($person->name === 'jiro')->toBeFalse()
+        ->and($person->age === 20)->toBeTrue();
 });
