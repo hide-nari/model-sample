5332b70a2bd9fd23cd58d7b815aa9b81d2a6d07d
watanabeHi <hide3@mild.ocn.ne.jp>
2025-12-27 03:15:32 +0900

    Mod reflection logic

--- src/ValidateNameLength.php
+++ src/ValidateNameLength.php
@@ -2,19 +2,23 @@
 
 namespace Hidenari\ModelSample;
 
+use ReflectionClass;
 use ReflectionProperty;
 
 trait ValidateNameLength
 {
     private function validateNameLength($name): void
     {
-        $ref = new ReflectionProperty(__CLASS__, 'name');
-        $attributes = $ref->getAttributes(ValidateLength::class);
-        foreach ($attributes as $attribute) {
-            if (mb_strlen($name) < $attribute->newInstance()->min
-                || mb_strlen($name) > $attribute->newInstance()->max
-            ) {
-                throw new \InvalidArgumentException('Invalid name string');
+        $refClass = new ReflectionClass(__CLASS__);
+        foreach ($refClass->getProperties() as $property) {
+            $refProperty = new ReflectionProperty(__CLASS__, $property->getName());
+            $attributes = $refProperty->getAttributes(ValidateLength::class);
+            foreach ($attributes as $attribute) {
+                if (mb_strlen($name) < $attribute->newInstance()->min
+                    || mb_strlen($name) > $attribute->newInstance()->max
+                ) {
+                    throw new \InvalidArgumentException('Invalid name string');
+                }
             }
         }
     }
