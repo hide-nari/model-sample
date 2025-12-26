b7803ea293d9f0095507091998c4cf896b225e8b
watanabeHi <hide3@mild.ocn.ne.jp>
2025-12-27 06:15:03 +0900

    Add attribute type

--- src/ValidateLength.php
+++ src/ValidateLength.php
@@ -7,5 +7,5 @@ use Attribute;
 #[Attribute]
 class ValidateLength
 {
-    public function __construct(public $min, public $max) {}
+    public function __construct(public int $min, public int $max) {}
 }
